9ec1f64fc719bc778b2f5eb71eb84864d53a2f4c
Andi DeVois <adevois@gmail.com>
2017-06-11 13:30:40 -0500

    Add project files.

---
+++ TakeItAndLoveIt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TakeItAndLoveIt.MvcApplication" Language="C#" %>
---
+++ TakeItAndLoveIt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TakeItAndLoveIt.Startup))]
+namespace TakeItAndLoveIt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
