f05837006544b3044df09043c4a011cbb7398004
Andi DeVois <adevois@gmail.com>
2017-06-11 20:50:45 -0500

    file setup, angular installed and tested

--- TakeItAndLoveIt/App_Start/BundleConfig.cs
+++ TakeItAndLoveIt/App_Start/BundleConfig.cs
@@ -26,6 +26,13 @@ namespace TakeItAndLoveIt
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                    "~/Scripts/angular.js",
+                    "~/Scripts/angular-route.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                    "~/app/app.js"));
         }
     }
 }
---
+++ TakeItAndLoveIt/app/app.js
@@ -0,0 +1,5 @@
+﻿var app = angular.module("JobLoveApp", ["ngRoute"]);
+
+          app.controller("homeController", ["$scope", function($scope) {
+                         $scope.welcome = "*waves* ";
+                     }]);
