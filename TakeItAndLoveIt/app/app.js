1dc26723a944f3dd3b485808a537814f926940ca
Andi DeVois <adevois@gmail.com>
2017-06-22 21:28:53 -0500

    Working on displays

--- TakeItAndLoveIt/app/app.js
+++ TakeItAndLoveIt/app/app.js
@@ -3,31 +3,36 @@
 angular.module("JobLoveApp")
     .config([
         "$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
-            $routeProvider
-                .when("/add",
-                    {
-                        templateUrl: "app/partials/add.html",
-                        controller: "addController"
-                    })
-                .when("/list",
-                    {
-                        templateUrl: "app/partials/list.html",
-                        controller: "listController"
-                    })
-                .when("/detail",
-                    {
-                        templateUrl: "app/partials/details.html",
-                        controller: "addController"
-                    })
-                .when("/detail/:id",
-                    {
-                        templateUrl: "app/partials/details.html",
-                        controller: "detailController"
-                    })
-            .when("/login",
-                    {
-                        templateUrl: "app/partials/login.html",
-                        controller: "loginController"
-                    });
+                $routeProvider
+                    .when("/add",
+                        {
+                            templateUrl: "app/partials/add.html",
+                            controller: "addController"
+                        })
+                    .when("/list",
+                        {
+                            templateUrl: "app/partials/list.html",
+                            controller: "listController"
+                        })
+                    .when("/detail",
+                        {
+                            templateUrl: "app/partials/details.html",
+                            controller: "addController"
+                        })
+                    .when("/detail/:id",
+                        {
+                            templateUrl: "app/partials/details.html",
+                            controller: "detailController"
+                        })
+                    .when("/main",
+                        {
+                            templateUrl: "app/partials/landing.html",
+                            controller: "homeController"
+                        })
+                    .when("/Home/Index",
+                        {
+                            templateUrl: "app/partials/landing.html",
+                            controller: "homeController"
+                        });
         }
     ]);
