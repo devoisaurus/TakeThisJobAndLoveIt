32a4dfd0dc583bd450ec07be85fbcd836409a6f2
Andi DeVois <adevois@gmail.com>
2017-06-23 00:25:25 -0500

    The styling sucks but it works

--- TakeItAndLoveIt/app/detailController.js
+++ TakeItAndLoveIt/app/detailController.js
@@ -18,7 +18,7 @@
         $scope.addInteraction = function (interaction) {
             $http.post("/api/companies/" + $routeParams.id + "/interactions/", $scope.interaction)
                 .then(function (success) {
-                        $location.path("/detail/:id");
+                        console.log("i did the thing");
                     },
                     function (error) {
                         console.log("I did not do the thing");
